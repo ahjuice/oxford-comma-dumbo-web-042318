04dddd9349e607e9e4b213fcadd4ffa10190f1ab
ahjuice <ahjuice@users.noreply.github.com>
2018-04-21 15:46:18 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0]
+  elsif array.count == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    combo_string = ""
+    array.each_with_index { | string, index |
+      if index == 0
+        combo_string << string
+      elsif index == array.count - 1
+        combo_string << ", and #{string}"
+      else
+        combo_string << ", #{string}"
+      end
+    }
+    return combo_string
+  end
 end
